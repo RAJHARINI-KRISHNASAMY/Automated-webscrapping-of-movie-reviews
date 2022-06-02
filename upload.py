63037057a9b911d7dc6e5f96a36c50f19b3fbf27
RAJHARINI-KRISHNASAMY <rajharini1997@gmail.com>
2022-06-02 13:09:09 -0400

    Update upload.py

--- upload.py
+++ upload.py
@@ -3,5 +3,5 @@ import pandas as pd
 df = pd.read_csv('movie.csv')
 df.reset_index(inplace=True)
 data = df.to_dict('records')
-db = MongoClient("mongodb://BDAT1004:Password@cluster0-shard-00-00.gzlae.mongodb.net:27017,cluster0-shard-00-01.gzlae.mongodb.net:27017,cluster0-shard-00-02.gzlae.mongodb.net:27017/movieReviewDatabase?ssl=true&replicaSet=atlas-qnmzak-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = MongoClient("")
 x = db["movieReviewDatabase"]["Pirates of the Caribbean"].insert_many(data)
